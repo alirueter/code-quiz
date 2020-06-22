050b222e1f96fbfecf15d5997e44792ce7906be9
alirueter <rueter.alison@gmail.com>
2020-06-21 19:19:32 -0500

    added css styles & added local storage functionality for user to save name and score

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,58 +1,122 @@
 var startBtn = document.getElementById("startBtn");
 var questionDiv = document.getElementById("questions");
-console.log(questionDiv);
 var currentQuestionIndex = 0;
+var timerEl = document.getElementById('countdown');
+var timer;
+var time = 100;
+var userInput = "";
+
 
 // The array of questions for our quiz game.
 var questions = [
     //questions from quizlet: https://quizlet.com/za/444626000/ict1512-javascript-exam-prep-truefalse-questions-flash-cards/
-    { q: 'The statement document.write("This is a text string."); prints "This is a text string".', a: 'f' },
+    { q: 'The statement document.("This is a text string."); prints "This is a text string".', a: 'f' },
     { q: 'You can use a text string as a literal value or assign it to a variable.', a: 't' },
     { q: 'A function must return a value.', a: 'f' },
     { q: 'Assignment operators are used to perform mathematical calculations, such as addition, subtraction, multiplication, and division.', a: 'f' },
     { q: 'In an infinite loop, a loop statement never ends because its conditional expression is never truthy.', a: 'f' }
 ];
-console.log(questions);
 
-// We start the game with a score of 0.
-var score = 0;
 
 
 let buttonsArray = document.querySelectorAll(".answerBtn");
-buttonsArray.forEach(function(elem) {
-    elem.addEventListener("click", function(){
+buttonsArray.forEach(function (elem) {
+    elem.addEventListener("click", function () {
+
         var question = document.getElementById("questionText");
-        question.innerHTML = questions[currentQuestionIndex].q;
-        var answer = elem.id
+        var answer = elem.id;
+        console.log(answer);
         console.log(questions[currentQuestionIndex]);
-    
+
+
         if (
             (answer === "true" && questions[currentQuestionIndex].a === 't') ||
             (answer === "false" && questions[currentQuestionIndex].a === 'f')
-            ) {
-            // Increase score
-            score++;
-            document.getElementById("answerCorrect").innerHTML = "Correct!";
-            
-            } else {
-                document.getElementById("answerCorrect").innerHTML = 'Wrong!';
-            }
+        ) {
 
-            if (questions.length > currentQuestionIndex){
-            currentQuestionIndex++
+            document.getElementById("answerCorrect").innerHTML = "Correct!";
+            currentQuestionIndex++;
+        } else {
+            time -= 15;
+
+            document.getElementById("answerCorrect").innerHTML = "Wrong!";
+            currentQuestionIndex++;
+        };
+        console.log("question: ", currentQuestionIndex)
+
+        if (currentQuestionIndex < questions.length) {
+
+            question.innerHTML = questions[currentQuestionIndex].q;
+        } else {
+            quizEnd()
         }
+
     });
 });
 
-
-var startQuiz = function(){   
-    currentQuestionIndex = 0;
+var startQuiz = function () {
     var question = document.getElementById("questionText");
     question.innerHTML = questions[currentQuestionIndex].q;
+    timer = setInterval(clock, 1000);
+
 }
 
+function quizEnd() {
+    console.log("quizend:", time)
+    questionDiv.innerHTML = ""
+    //this goes in the function that you end your quiz with
+    clearInterval(timer);
+    timerEl.textContent = time;
+    //Show total at end
+    document.getElementById('quizScore').innerHTML = 'Score: ' + time;
 
-// Show total at end
-document.getElementById('main').innerHTML = 'You got ' + score + '/' + questions.length;
+};
+
+function clock() {
+    // update time
+    time--;
+    timerEl.textContent = time;
+
+    // check if user ran out of time
+    if (time <= 0) {
+        clearInterval(timer);
+        console.log(time);
+    }
+};
+
+function getHighScores(){
+    console.log("All the scores: ",localStorage)
+
+
+}
+
+function getScore(forWho) {
+
+    return localStorage.getItem(forWho)
+
+};//end getScore() fct def
+
+function storeScore() {
+
+    localStorage.setItem( userInput , time.toString());
+    //localStorage.setItem("Users Input", userInput.toString());
+};
+
+document.getElementById('highscoreInput').innerHTML = ` 
+    Enter your name:
+    <input type="text" id="initialInput"></input>
+    <button id="saveScore" class="saveButton">Save Score</button>
+    `
+var initialInputEl = document.getElementById("initialInput");
+
+document.getElementById("saveScore").addEventListener("click", function () {
+    userInput = initialInputEl.value;
+    console.log("User input: ", userInput);
+
+    storeScore()
+});
+
+
 
 startBtn.onclick = startQuiz;
+getScore();
